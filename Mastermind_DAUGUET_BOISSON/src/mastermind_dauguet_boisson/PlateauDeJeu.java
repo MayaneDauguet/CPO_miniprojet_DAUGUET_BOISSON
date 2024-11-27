4edec2684bfc1eed4979d89e0f02f0729df7890f
Inès Boisson <ines.boisson@epfedu.fr>
2024-11-27 18:11:04 +0100

    debut classe PlateauDeJeu, à revoir

--- Mastermind_DAUGUET_BOISSON/src/mastermind_dauguet_boisson/PlateauDeJeu.java
+++ Mastermind_DAUGUET_BOISSON/src/mastermind_dauguet_boisson/PlateauDeJeu.java
@@ -4,10 +4,45 @@
  */
 package mastermind_dauguet_boisson;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author daugu
  */
 public class PlateauDeJeu {
-    
+    private Combinaison combinaisonSecrete;
+    private int nbToursMax;
+    private  ArrayList <Combinaison> tentatives;
+    private  ArrayList <String> reponses;
+
+    public PlateauDeJeu(Combinaison combinaisonSecrete, int nbToursMax, ArrayList<Combinaison> tentatives, ArrayList<String> reponses) {
+        this.combinaisonSecrete = combinaisonSecrete;
+        this.nbToursMax = nbToursMax;
+        this.tentatives = tentatives;
+        this.reponses = reponses;
+    }
+    public void proposerCombinaison(Combinaison tentatives){
+        tentatives.add(tentatives);
+        int rouges = 0; // Bons éléments bien placés
+        int blancs = 0; // Bons éléments mal placés
+        for (int i = 0; i < 4; i++) {
+            if (combinaisonSecrete.get(i) == tentatives.get(i)) {
+               rouges++;
+            } else if (combinaisonSecrete.contient(tentatives.get(i))) {
+               blancs++;
+        }
+    }
+
+
+    }
+    public void afficherPlateau(){
+        
+    }
+    public void estVictoire(){
+        
+    }
+    public void estDefaite(){
+        
+    }
 }
